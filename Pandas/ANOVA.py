d170fa2bd588a1c010b3eb1d9612bb5c398fc105
omel1311 <omel1311@gmail.com>
2023-07-21 11:40:59 +0300

    display.max_rows display.max_columns display.width'

--- Pandas/ANOVA.py
+++ Pandas/ANOVA.py
@@ -4,16 +4,24 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import seaborn as sb
 
+
+
 url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv'
 
 df = pd.read_csv(url)
 df.replace('?', np.NAN, inplace=True)
 
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_rows', 500)
+
+print(df.head())
+
 df_gptest = df[['drive-wheels','body-style','price']]
 print(df_gptest.head())
 
 grouped_test2=df_gptest[['drive-wheels', 'price']].groupby(['drive-wheels'])
-grouped_test2.head(2)
+print(grouped_test2.head())
 
 f_val, p_val = stats.f_oneway(grouped_test2.get_group('fwd')['price'], grouped_test2.get_group('rwd')['price'],
                               grouped_test2.get_group('4wd')['price'])
