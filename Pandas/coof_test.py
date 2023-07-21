ea3ef7113842cc901962b8025271718ecd638609
omel1311 <omel1311@gmail.com>
2023-07-21 11:52:05 +0300

    np.format_float_positional

--- Pandas/coof_test.py
+++ Pandas/coof_test.py
@@ -7,6 +7,7 @@ url= "C:\\Users\\0487\\Desktop\\Омельяненко\\кпд\\зп.xlsx"
 
 df = pd.read_excel(url, sheet_name='python_3')
 print(df.info())
+
 coof, P_value = stats.pearsonr(df['prise'], df['year'])
 P_value1 = np.format_float_positional(P_value, trim='-')
 print('coof = ', coof, 'P_value = ', P_value1 )
