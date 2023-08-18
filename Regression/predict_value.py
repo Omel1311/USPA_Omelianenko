b215611f0fb1cd02481e69125982a15fcf58f651
omel1311 <omel1311@gmail.com>
2023-08-18 16:50:12 +0300

    predict

--- Regression/predict_value.py
+++ Regression/predict_value.py
@@ -5,13 +5,9 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_rows', 500)
-
-
 
 url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv'
 
@@ -26,7 +22,6 @@ Y = df['price']
 lm.fit(X,Y)
 
 Yhat=lm.predict(X)
-
 print(lm.coef_)
 print(lm.intercept_)
 
@@ -41,44 +36,52 @@ lm2.fit(df[['normalized-losses' , 'highway-mpg']],df['price'])
 print(lm2.coef_)
 print(lm2.intercept_)
 
+
+# Regression Plot
 width = 12
 height = 10
+# plt.figure(figsize=(width, height))
+# sns.regplot(x="highway-mpg", y="price", data=df)
+# plt.ylim(0,)
+# plt.show()
+#
+# plt.figure(figsize=(width, height))
+# sns.regplot(x="peak-rpm", y="price", data=df)
+# plt.ylim(0,)
+# plt.show()
+
+
+
+#  Verify correlation
+# print('corr', df[["peak-rpm","highway-mpg","price"]].corr())
+# sns.set (rc = {'figure.figsize':(8, 8)})
+# dataplot = sns.heatmap(df[["peak-rpm","highway-mpg","price"]].corr(), cmap="YlGnBu", annot=True)
+# plt.show()
+
+
+
+#  Residual Plot
+# width = 12
+# height = 10
+# plt.figure(figsize=(width, height))
+# sns.residplot(x=df['highway-mpg'],y=df['price'])
+# plt.show()
+
+
+
+#   Multiple Linear Regression
+Y_hat=lm.predict(Z)
 plt.figure(figsize=(width, height))
-sns.regplot(x="highway-mpg", y="price", data=df)
-plt.ylim(0,)
+
+ax1 = sns.distplot(df['price'], hist=False, color="r", label="Actual Value")
+sns.distplot(Y_hat, hist=False, color="b", label="Fitted Values" , ax=ax1)
+
+plt.title('Actual vs Fitted Values for Price')
+plt.xlabel('Price (in dollars)')
+plt.ylabel('Proportion of Cars')
+
 plt.show()
-
-plt.figure(figsize=(width, height))
-sns.regplot(x="peak-rpm", y="price", data=df)
-plt.ylim(0,)
-plt.show()
-
-
-print('corr', df[["peak-rpm","highway-mpg","price"]].corr())
-
-sns.set (rc = {'figure.figsize':(8, 8)})
-dataplot = sns.heatmap(df[["peak-rpm","highway-mpg","price"]].corr(), cmap="YlGnBu", annot=True)
-plt.show()
-# X=df[['sales']]
-# y= df['advertising costs']
-#
-# model.fit(X,y)
-#
-# new_x = pd.DataFrame({'sales': [10]})
-#
-# Y_predict = model.predict(new_x)
-#
-# r2 = model.score(X,y)
-#
-#
-# print('Y_predict: ', Y_predict)
-# print('r2: ', r2)
-#
-# coof, P_value = stats.pearsonr(df['advertising costs'], df['sales'])
-# P_value1 = np.format_float_positional(P_value, trim='-')
-# print('coof = ', coof, 'P_value = ', P_value1 )
-#
-
+plt.close()
 
 
 
