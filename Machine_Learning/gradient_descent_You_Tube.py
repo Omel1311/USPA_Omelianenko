e81a6fb3f9cf6426a86b6bbc9b9b1c2ede26d8e7
omel1311 <omel1311@gmail.com>
2024-04-30 16:38:51 +0300

    РПАРc

--- Machine_Learning/gradient_descent_You_Tube.py
+++ Machine_Learning/gradient_descent_You_Tube.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def objective(x):
-    return x**2.0
+    return x**9.0
 
 
 bounds = np.array([[-10.0, 10.0]])
@@ -13,12 +13,13 @@ inputs = []
 outputs = []
 
 for i in range(1000):
-  x = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
-  y = objective(x)
-  inputs.append(x)
-  outputs.append(y)
+    x = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+    y = objective(x)
+    inputs.append(x)
+    outputs.append(y)
 
 
+#    функция для опередлеения градиента (скорости измененения наклона)
 def derivative(x):
     return x * 2.0
 
@@ -40,6 +41,34 @@ for i in range(20):
     print('>%d f(%s) = %.5f' % (i, x, x_evaluation))
 
 
+# plt.scatter(inputs, outputs)
+# plt.scatter(solutions, evaluations, color='r')
+# plt.show()
+
+
+#  Отображение сразу двух граффиков
+plt.figure(figsize=(10, 5))  # Задаем размер окна для графиков
+
+# Первый график
+plt.subplot(1, 3, 1)  # (количество строк, количество столбцов, индекс графика)
+plt.scatter(inputs, outputs, color='blue')
+plt.title('Inputs vs. Outputs')
+plt.xlabel('Inputs')
+plt.ylabel('Outputs')
+
+# Второй график
+plt.subplot(1, 3, 2)
+plt.scatter(solutions, evaluations, color='red')
+plt.title('Solutions vs. Evaluations')
+plt.xlabel('Solutions')
+plt.ylabel('Evaluations')
+
+
+plt.subplot(1, 3, 3)
 plt.scatter(inputs, outputs)
 plt.scatter(solutions, evaluations, color='r')
+plt.show()
+
+# Отображение обоих графиков
+plt.tight_layout()  # Эта функция помогает предотвратить перекрытие содержимого графиков
 plt.show()
